8e1f123ee311ac47e69826818e307da68b06b3d5
AI Profile <aiprofile.it@gmail.com>
2026-03-28 08:35:46 +0700

    Deploy website - based on 57d4fa0c1caeb6921f208d5d563dccb528ec2a49

---
+++ assets/js/acecf23e.55bd47a6.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkwebsite=globalThis.webpackChunkwebsite||[]).push([[1903],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/notes/blog","blogTitle":"Blog","authorsListPath":"/notes/blog/authors"}')}}]);
